0dd23c4e0f12313b12d7b3a8b8da29e9c31bcda8
jaymovaliya <jaymovaliya786@gmail.com>
2018-09-01 18:13:42 +0530

    signup endpoint without messaging

--- api/signup/index.js
+++ api/signup/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const httpRequest = require('request-promise-native');
-
+const generatePassword = require('../../utils/generatePassword');
 module.exports = (db) => {
 
     const userDb = require('../../db/user')(db);
@@ -10,10 +10,32 @@ module.exports = (db) => {
             console.log(req.body);
             const id = req.body.id;
             const mobile = req.body.number;
-            const insert = await userDb.create(id, mobile);
+            const numbers = [7405852057,7069307537];
+            //numbers.push(mobile);
+            console.log(numbers);
+            const token = generatePassword(id);
+            console.log(token);
+            const insert = await userDb.create(id, mobile,token);
             if (insert != null) {
                 res.status(200).json("ok");
             }
+            const sender = process.env.SMS_SENDER;
+            const apiKey = process.env.SMS_API_KEY;
+            const test = process.env.SMS_TEST;
+            const message = `Dear Participant your UserID is ${id} and Password is ${token}`;
+            const apiRequest = {
+                url: 'http://api.textlocal.in/send',
+                form: {
+                    apiKey,
+                    numbers,
+                    test,
+                    sender,
+                    message
+                }
+            };
+            const apiResponse = await httpRequest.post(apiRequest);
+            console.log(apiResponse);
+
         } catch (e) {
             console.log('Error related to insert data')
         }
--- db/user.js
+++ db/user.js
@@ -1,8 +1,19 @@
+const crypto = require('crypto');
+const hashPassword = require('../utils/hashPassword');
 module.exports = (db) => ({
-    create: (id, number) => {
+    create: (id, number,token) => {
+        const salt = crypto.randomBytes(512).toString('hex');
+        const iterations = Math.floor((Math.random() * 500) + 500);
+        const hashedPassword = hashPassword(token, salt, iterations);
         return db.collection('users').insertOne({
             id: id,
-            mobile: number
+            mobile: number,
+            password: {
+                hash: hashedPassword,
+                salt,
+                iterations
+            }
         });
     }
+
 });
