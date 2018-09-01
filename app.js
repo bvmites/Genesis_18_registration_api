83b33faf578638d67de323c5d293448f4f7dbb0f
rushi7997 <ramirushi7997@gmail.com>
2018-09-01 13:58:22 +0530

    first commit

--- app.js
+++ app.js
@@ -1,41 +1,73 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const MongoClient = require('mongodb').MongoClient;
+const bodyparser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const signup = require("./api/signup");
+const user = require("./api/user");
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const server = require('http').createServer(app);
+server.listen(4000);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+dotenv.config();
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+(async () => {
+    try{
+        const client = await MongoClient.connect(process.env.DB);
+        console.log("Connected to database");
+        const db = client.db('Genesis-18');
+        app.use('/signup',signup(db));
+        app.use(function(req, res, next) {
+            let err = new Error('Not Found');
+            err.status = 404;
+            next(err);
+        });
+        app.use(function(err, req, res, next) {
+            // set locals, only providing error in development
+            res.locals.message = err.message;
+            res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+            // render the error page
+            res.status(err.status || 500);
+            res.render('error');
+        });
+
+    }
+    catch (e) {
+        console.log("Error");
+    }
+})();
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   let err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
