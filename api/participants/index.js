0da7f412728c9d0769cc6cc1ac5a63b60d8f0f4b
jaymovaliya <jaymovaliya786@gmail.com>
2018-09-02 19:01:37 +0530

    update participant

--- api/participants/index.js
+++ api/participants/index.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const generateToken = require('../../utils/generateToken');
+const httpRequest = require('request-promise-native');
 
 module.exports = (db) => {
     const participantDB = require('../../db/participant')(db);
 
     //POST participant/:id
-    router.post('/',async (request, response) => {
+    router.post('/', async (request, response) => {
         try {
-            new_id = request.body.id;
-            newToken = generateToken(request.body.id);
+            const new_id = request.body.id;
+            const newToken = generateToken(request.body.id);
+            console.log(newToken);
             let newOrder = {
                 "events": request.body.events,
                 "sum": request.body.sum,
@@ -16,10 +18,31 @@ module.exports = (db) => {
                 "paid": false
             };
             const newParticipant = await participantDB.get(new_id);
+            console.log(newParticipant);
             newParticipant.orders.push(newOrder);
-            const number = newParticipant.number;
+            const number = newParticipant.phone;
+            console.log(number);
+            const numbers = JSON.stringify(number);
+            console.log(numbers);
             await participantDB.replace(new_id, newParticipant);
-            response.status(200).json({message:"success"});
+            response.status(200).json({message: "success"});
+            const sender = process.env.SMS_SENDER;
+            const apiKey = process.env.SMS_API_KEY;
+            const test = process.env.SMS_TEST;
+            const message = `Dear Event Manager, Your credentials for accessing the SMS portal for the event xyz are Username: ${new_id}, Password: ${newToken}
+Team Udaan`;
+            const apiRequest = {
+                url: 'http://api.textlocal.in/send',
+                form: {
+                    apiKey,
+                    numbers,
+                    test,
+                    sender,
+                    message
+                }
+            };
+            const apiResponse = await httpRequest.post(apiRequest);
+            console.log(apiResponse);
         }
         catch (e) {
             console.log(e)
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,9 +1,13 @@
 const crypto = require('crypto');
+const hashPassword = require('./hashPassword');
 
 module.exports = (username) => {
+    const salt = crypto.randomBytes(512).toString('hex');
+    const iterations = Math.floor((Math.random() * 500) + 500);
+    const hashedPassword = hashPassword(username, salt, iterations);
     const key = process.env.HASH_SECRET;
     return crypto.createHmac('sha512', key)
-        .update(username)
+        .update(hashedPassword)
         .digest('hex')
         .toString()
         .slice(0, 6);
